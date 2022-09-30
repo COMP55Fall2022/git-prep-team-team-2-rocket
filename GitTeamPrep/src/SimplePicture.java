5c5f6e783fb13770e90260c2c6579fe0c1b17c22
Divine <d_atah@u.pacific.edu>
2022-09-30 16:09:33 -0700

    changed text name for glabel

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -15,7 +15,7 @@ public class SimplePicture extends GraphicsProgram {
 		//try 2
 		GImage robot = new GImage(ROBOTIMAGENAME, 200, 200);
 		add(robot);
-		GLabel label = new GLabel("asldfjaisopaiwdfn", 200, 400);
+		GLabel label = new GLabel("asdfghj", 200, 400);
 		add(label);
 	}
 	
